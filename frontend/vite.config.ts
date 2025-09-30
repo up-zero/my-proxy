e0e22bd0bd50ba38e95b4a305bfff81bc2252a81
maomaochong <526338420@qq.com>
2025-09-30 11:29:56 +0800

    👼 add keep copy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: ".gitkeep",
+          dest: "", // 复制到 dist 根目录
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
